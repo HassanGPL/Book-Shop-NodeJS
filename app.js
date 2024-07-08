66a521def6943b974bdcd677c4b944167aad4990
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-08 14:58:49 +0300

    Send Response

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>NODEJS Application</title></head>');
+    res.write('<body><h1>Hello to my first NodeJS application</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
