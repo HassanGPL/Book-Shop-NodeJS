cc2ca043eaa7121e040f08940193507a6946c32b
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-09 17:13:34 +0300

    Add routes file to work with requests

--- app.js
+++ app.js
@@ -1,48 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const { url, method } = req;
-    if (url === '/') {
+const routes = require('./routes');
 
-        res.write('<html>');
-        res.write('<head><title>NODEJS Application</title></head>');
-        res.write('<body>');
-        res.write('<form action="/message" method="POST">');
-        res.write('<input type="text" name="message">');
-        res.write('<button method="submit">Save</button>');
-        res.write('</form>');
-        res.write('</body>');
-        res.write('</html>');
-
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const data = [];
-        req.on('data', (chunk) => {
-            data.push(chunk);
-            console.log(chunk);
-        });
-
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(data).toString();
-            const message = parsedBody.split('=')[1];
-            console.log(message);
-            fs.writeFile('message.txt', message, (e) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/')
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>NODEJS Application</title></head>');
-    res.write('<body><h1>Hello to my first NodeJS application</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes.requestHandler);
 
 server.listen(3000);
