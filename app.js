1312e64ecaf260dbdd4b017d26a41abe7afc7f53
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-11 21:14:28 +0300

    Working on requests routes

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Middleware 1');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', (req, res, next) => {
+    console.log('This will always runs!');
     next();
 });
 
-app.use((req, res, next) => {
-    console.log('Middleware 2');
-    res.send('<h>Hello World with Express!</h>')
+app.get('/add-product', (req, res, next) => {
+    console.log('products middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">save</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body.title);
+    res.redirect('/');
+});
+
+app.get('/', (req, res, next) => {
+    console.log('Main middleware');
+    res.send('<h1>Hello World from Express!</h1>');
 });
 
 app.listen(3000);
