db2bf8d0508dcb19a4a85a1b6d39a4a7e167099a
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-11 19:56:06 +0300

    start working with middlewares

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Middleware 1');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Middleware 2');
+    res.send('<h>Hello World with Express!</h>')
+});
+
 const server = http.createServer(app);
 
 const port = 3000;
