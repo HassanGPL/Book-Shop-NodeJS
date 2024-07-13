206a9dd72cb22f8349e5187c754b930989ab102e
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-13 19:48:50 +0300

    Working with Pug tamplate engine

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const adminData = require('./routes/admin');
 
 const app = express();
 
+app.set('view engine', 'pug');
+// app.set('views','views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,7 +22,7 @@ app.use('/admin', adminData.router);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'))
+    res.status(404).render('404', { title: 'Page not found' });
 });
 
 app.listen(3000);
