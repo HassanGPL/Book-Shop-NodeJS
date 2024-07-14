1c1f2c7e6891fb3b9c80d5d70e4560df16f2a5ac
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-14 19:17:59 +0300

    Working with ejs tamplate engine

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const adminData = require('./routes/admin');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 // app.set('views','views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,7 +22,7 @@ app.use('/admin', adminData.router);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { title: 'Page not found' });
+    res.status(404).render('404', { title: 'Page not found', path: '' });
 });
 
 app.listen(3000);
