3da3042494e5107dba389dd7d4cf881f01cd7a02
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-17 19:03:13 +0300

    Add product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,20 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', { title: 'Add Product', path: '/admin/add-product' });
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title })
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', { products: products, title: 'Shop', path: '/' })
+    const products = Product.fetchAll();
+    res.render('shop', {
+        products: products,
+        title: 'Shop',
+        path: '/'
+    })
 }
