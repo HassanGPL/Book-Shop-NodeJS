35ea37a5a6ee9fcafed5c54c5dc7aefce8bf68df
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-18 19:49:41 +0300

    Add Orders item

--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,11 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 
-router.get('/products', shopController.getProducts)
+router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
 
