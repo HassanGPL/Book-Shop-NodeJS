a479bdae45bd8909b4c023dd7e18a59f832d8aeb
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-20 17:59:17 +0300

    Add id to each product and add details page to view each page

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,6 +20,17 @@ exports.getProducts = (req, res, next) => {
     });
 }
 
+exports.getProduct = (req, res, next) => {
+    const productId = req.params.productId;
+    Product.findById(productId, product => {
+        res.render('shop/product-details', {
+            product: product,
+            title: product.title,
+            path: '/products'
+        });
+    });
+}
+
 exports.getCart = (req, res, next) => {
     res.render('shop/cart', {
         title: 'Cart',
