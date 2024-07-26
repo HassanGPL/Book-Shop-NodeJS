969071e6c26e91353107a489cdf671a7aeb5118a
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-26 03:27:25 +0300

    connecting with mysql database

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./utils/database');
 
 const express = require('express');
-
-const shopRouter = require('./routes/shop');
-const adminRouter = require('./routes/admin');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-// app.set('views','views');
+app.set('views', 'views');
+
+const shopRouter = require('./routes/shop');
+const adminRouter = require('./routes/admin');
+
+db.execute('SELECT * FROM products')
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
