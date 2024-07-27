1f1e9dbcae79ab69b4307f517b577c96ee829c41
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-27 23:50:18 +0300

    Define product model

--- models/product.js
+++ models/product.js
@@ -1,39 +1,30 @@
-const db = require('../utils/database');
-const Cart = require('./cart');
+const Sequelize = require('sequelize');
 
+const sequelize = require('../utils/database');
 
-
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
+});
 
-    save() {
-        return db.execute('INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-            [
-                this.title,
-                this.price,
-                this.description,
-                this.imageUrl
-            ])
-    }
-
-    static deleteById(id) {
-
-    }
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products');
-    }
-
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE products.id = ?',
-            [
-                id
-            ])
-    }
-} 
+module.exports = Product;
