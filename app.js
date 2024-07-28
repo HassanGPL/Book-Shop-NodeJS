2edee08b19c271c7dec15722183236e166f4593a
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-28 21:27:42 +0300

    Add one to many relationship

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./utils/database');
 
+const User = require('./models/user');
+const Product = require('./models/product');
+
 const express = require('express');
 
 const app = express();
@@ -26,8 +29,11 @@ app.use(shopRouter);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constrains: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
-    .sync()
+    .sync({ force: true })
     .then(result => {
         app.listen(3000);
     })
