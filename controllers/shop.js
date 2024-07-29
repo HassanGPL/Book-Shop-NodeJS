dcaee53024378cfb97ea9d02f2584d786c1ee5cb
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-29 18:15:25 +0300

    Enhance Add to cart functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -70,21 +70,15 @@ exports.postCart = (req, res, next) => {
             return cart.getProducts({ where: { id: productId } })
         })
         .then(products => {
-            let product;
-            if (products.length > 0) {
-                product = products[0];
-            }
+            let product = products[0];
             if (product) {
-                const oldQuantity = product.cartItem.quantity;
-                newQuantity = oldQuantity + 1;
-                return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
+                newQuantity = product.cartItem.quantity + 1;
+                return product;
             }
-            return Product
-                .findByPk(productId)
-                .then(product => {
-                    return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
-                })
-                .catch(err => console.log(err));
+            return Product.findByPk(productId)
+        })
+        .then(product => {
+            return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
         })
         .then(() => {
             res.redirect('/cart');
