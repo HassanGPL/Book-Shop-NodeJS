d6c076e3dfcf3934cccf6c3e2e3a7f80321c0f5d
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-29 11:02:23 +0300

    Use association method to get products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll()
+    req.user.getProducts()
         .then(products => {
             res.render('admin/products', {
                 products: products,
@@ -49,8 +49,9 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const productId = req.params.productId;
-    Product.findByPk(productId)
-        .then(product => {
+    req.user.getProducts({ where: { id: productId } })
+        .then(products => {
+            const product = products[0];
             if (!product) {
                 return res.redirect('/');
             }
