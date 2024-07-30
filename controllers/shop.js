e1bd393dd9e208c6b3413fabcd8a3e0e901cfb92
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-07-30 17:56:53 +0300

    Order functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -110,6 +110,31 @@ exports.getOrders = (req, res, next) => {
     });
 }
 
+exports.postOrder = (req, res, next) => {
+    req.user
+        .getCart()
+        .then(cart => {
+            return cart.getProducts();
+        })
+        .then(products => {
+            return req.user
+                .createOrder()
+                .then(order => {
+                    return order.addProducts(
+                        products.map(product => {
+                            product.orderItem = { quantity: product.cartItem.quantity };
+                            return product;
+                        })
+                    )
+                })
+                .catch(err => console.log(err));
+        })
+        .then(() => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
+}
+
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         title: 'Checkout',
