76c35d479d1593d966db93554741675312657a70
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-08-06 01:48:33 +0300

    Create User model and save function

--- models/user.js
+++ models/user.js
@@ -1,22 +1,22 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../utils/database').getDb;
 
-const sequelize = require('../utils/database');
-
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false
+class User {
+    constructor(username, email) {
+        this.username = username;
+        this.email = email;
     }
-});
+
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        const userMongoId = new mongodb.ObjectId(userId);
+        return db.collection('users').findOne({ _id: userMongoId });
+    }
+}
 
 module.exports = User;
