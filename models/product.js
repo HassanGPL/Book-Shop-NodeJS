0d3d1f435142f476b9831f78a5744ad169a04e6e
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-08-11 09:21:24 +0300

    Create Product model using mongoose

--- models/product.js
+++ models/product.js
@@ -1,3 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
+
 // const mongodb = require('mongodb');
 // const getDb = require('../utils/database').getDb;
 
