2aaff5a0aa3735649f5dadea436e430c234396b6
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-10-18 15:00:29 +0300

    Update error handler middleware

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+app.use((req, res, next) => {
     if (!req.session.user) {
         return next();
     }
@@ -51,14 +57,8 @@ app.use((req, res, next) => {
             next();
         })
         .catch(err => {
-            throw new Error(err);
+            next(new Error(err));
         });
-});
-
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
 });
 
 app.use('/admin', adminRouter);
@@ -69,7 +69,10 @@ app.use('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    res.redirect('/500');
+    res.status(500).render('500', {
+        title: 'Error!',
+        path: ''
+    });
 })
 
 mongoose.connect(MONGO_URI)
