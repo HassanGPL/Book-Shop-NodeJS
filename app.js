9999dae94d591b90dee4162f71142aad755eba46
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-11-02 10:57:13 +0200

    Serving Images Statically

--- app.js
+++ app.js
@@ -52,7 +52,10 @@ const authRouter = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: multerStorage, fileFilter: fileFilter }).single('image'));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 app.use(session({ secret: 'Hassan Ahmed', resave: false, saveUninitialized: false, store: store }));
 
